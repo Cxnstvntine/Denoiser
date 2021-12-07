afaca6ad21e29478237e4aed0107b2c0d6af56e7
VAKHANDER <72333765+VAKHANDER@users.noreply.github.com>
2021-12-07 03:51:02 +0300

    Update App.xaml.cs

--- Denoiser/Denoiser/App.xaml.cs
+++ Denoiser/Denoiser/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -16,32 +16,6 @@ namespace Denoiser
 {
     public partial class App : Application
     {
-        public static Bitmap Denoize(Bitmap scrBitmap)
-        {    
-            //Среднее значение цвета вокруг пикселя
-            Color averageColor = (Color.Black);
-            for (int i = 0; i < scrBitmap.Width; i++)
-            {
-                for (int j = 0; j < scrBitmap.Height; j++)
-                {
-                    averageColor = findAveragePixel();
-                    //scrBitmap.pixel = averageColor;
-                }
-            }            
-            return scrBitmap;
-        }
-
-        private static int findAveragePixel(Bitmap scrBitmap)
-        {
-            int count = 0;
-            int average = 0;
-            int Sum = 0;
-            //for x+-delta:
-                //for y+-delta:
-                    //Sum += scrBitmap.Pixel(x, y)
-                    //count ++
-            average = Sum / count;
-            return average;
-        }
+        
     }
 }
