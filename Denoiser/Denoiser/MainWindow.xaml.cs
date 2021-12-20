150d26247a8ee47ca0ff1df22be9643ae57da096
Constantine <72945585+cxnstvntine@users.noreply.github.com>
2021-12-20 06:34:08 +0300

    Подключил System.Drawing.Bitmap

--- Denoiser/Denoiser/MainWindow.xaml.cs
+++ Denoiser/Denoiser/MainWindow.xaml.cs
@@ -24,7 +24,6 @@ namespace Denoiser
         public MainWindow()
         {
             InitializeComponent();
-            string zalupa;
         }
         
         private void btnSave_Click(object sender, RoutedEventArgs e)
