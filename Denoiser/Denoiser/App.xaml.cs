5476298345025e9c75df59a6c75512f3ae39f3f3
Master <constantine.cxnstantine@gmail.com>
2021-12-23 15:16:04 +0300

    Автосохранение

--- Denoiser/Denoiser/App.xaml.cs
+++ Denoiser/Denoiser/App.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows;
 using System.Drawing;
 using System.Windows.Controls;
@@ -7,13 +8,14 @@ namespace Denoiser
 {
     public partial class App : Application
     {
-        public static Bitmap Denoize(System.Drawing.Bitmap scrBitmap)
+       
+        public static Bitmap Denoise(Bitmap scrBitmap)
         {
-            for (int i = 0; i < scrBitmap.Width; i++)
+            for (var i = 0; i < scrBitmap.Width; i++)
             {
-                for (int j = 0; j < scrBitmap.Height; j++)
+                for (var j = 0; j < scrBitmap.Height; j++)
                 {
-                    if (Broken(scrBitmap.GetPixel(i, j), i , j, scrBitmap))
+                    if(Broken(scrBitmap.GetPixel(i, j), i , j, scrBitmap))
                     {
                         //Среднее значение цвета вокруг пикселя
                         Color averageColor = FindAveragePixel(scrBitmap,i, j); 
@@ -31,17 +33,17 @@ namespace Denoiser
         //Ищем среднее значение пикселей вокруг
         private static Color FindAveragePixel(Bitmap scrBitmap, int i, int j)
         {
-            int Radius = 4;
-            int count = 0;
+            const int radius = 1;
+            var count = 0;
             int a = 0, r = 0, g = 0, b = 0;
             //Если это не один из крайних пикселей
             if (i + 1 <= scrBitmap.Height)
             {
-                for (int x = i; x < i + Radius; x++)
+                for (var x = i; x < i + radius; x++)
                 {
                     if (j + 1 <= scrBitmap.Width)
                     {
-                        for (int y = j; y < j + Radius; j++)
+                        for (var y = j; y < j + radius; j++)
                         {
                             a += scrBitmap.GetPixel(x, y).A;
                             r += scrBitmap.GetPixel(x, y).R;
@@ -54,7 +56,7 @@ namespace Denoiser
                     //Если справа не дошли до края, но находимся внизу картинки
                     else
                     {
-                        for (int y = j; y < scrBitmap.Width; j++)
+                        for (var y = j; y < scrBitmap.Width; j++)
                         {
                             a += scrBitmap.GetPixel(x, y).A;
                             r += scrBitmap.GetPixel(x, y).R;
@@ -67,11 +69,11 @@ namespace Denoiser
             }
             else
             {
-                for (int x = i; x < scrBitmap.Height; x++)
+                for (var x = i; x < scrBitmap.Height; x++)
                 {
                     if (j + 1 <= scrBitmap.Width)
                     {
-                        for (int y = j; y < j + Radius; j++)
+                        for (var y = j; y < j + radius; j++)
                         {
                             a += scrBitmap.GetPixel(x, y).A;
                             r += scrBitmap.GetPixel(x, y).R;
@@ -82,7 +84,7 @@ namespace Denoiser
                     }
                     else
                     {
-                        for (int y = j; y < scrBitmap.Width; j++)
+                        for (var y = j; y < scrBitmap.Width; j++)
                         {
                             a += scrBitmap.GetPixel(x, y).A;
                             r += scrBitmap.GetPixel(x, y).R;
@@ -100,7 +102,7 @@ namespace Denoiser
                 r /= count;
                 g /= count;
                 b /= count;
-                Color average = Color.FromArgb(a, r, g, b);
+                var average = Color.FromArgb(a, r, g, b);
                 return average;
             }
 
@@ -109,14 +111,12 @@ namespace Denoiser
 
         private static bool Broken(Color color, int x, int y, Bitmap scrBitmap)
         {
-            Color averageColor = FindAveragePixel(scrBitmap, x, y);
+            var averageColor = FindAveragePixel(scrBitmap, x, y);
             //Если значение полей цвета пикселя не больше и не меньше среднего, то вовзращаем true
-            if (color.A >= averageColor.A + 10 || color.R >= averageColor.R + 10 || color.G >= averageColor.G + 10 ||
-                color.B >= averageColor.B + 10)
-            {
-                return true;
-            }
-            return false;
+            return (color.A >= averageColor.A + 10 || color.R >= averageColor.R + 10 ||
+                    color.G >= averageColor.G + 10 ||
+                    color.B >= averageColor.B + 10);
         }
+        
     }
 }
